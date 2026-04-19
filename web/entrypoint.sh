9411834b9c47e5053c10decbbe5715548dd0978c
aanseaa product <aanseaa.product@gmail.com>
2026-04-19 05:36:21 +0000

    Updated entrypoint.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -21,8 +21,8 @@ fi
 
 # Check if CLICKHOUSE_URL is not set
 if [ -z "$CLICKHOUSE_URL" ]; then
-    echo "Warning: CLICKHOUSE_URL is not configured. Migrating from V2? Check out migration guide: https://langfuse.com/self-hosting/upgrade-guides/upgrade-v2-to-v3"
-    #exit 1
+    echo "Error: CLICKHOUSE_URL is not configured. Migrating from V2? Check out migration guide: https://langfuse.com/self-hosting/upgrade-guides/upgrade-v2-to-v3"
+    exit 1
 fi
 
 # Set DIRECT_URL to the value of DATABASE_URL if it is not set, required for migrations
